f32ecdad24f3b82bc3adb5cb7361abdaed13ad32
Austin Michael Regan <austinmregan@gmail.com>
2019-09-10 14:14:12 -0400

    added superscript text for homepage

--- MartianWeatherAppMobile/src/components/Home.js
+++ MartianWeatherAppMobile/src/components/Home.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 export default class HomeComponent extends Component {
   render() {
     return (
       <View>
-        <Text>HOME</Text>
+        <View style={{flexDirection: 'row', marginBottom: 20}}>
+          <Text style={{fontSize: 20, lineHeight: 30}}>4</Text>
+          <Text style={{fontSize: 15, lineHeight: 18}}>th</Text>
+          <Text style={{fontSize: 20, lineHeight: 30}}> Rock Weather</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  homeTitle: {
+    backgroundColor: Colors.lighter,
+  },
+});
