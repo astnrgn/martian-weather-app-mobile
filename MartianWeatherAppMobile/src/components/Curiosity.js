5e0ef11a284e6b4b4f7d4cbf466eb5c328dc6ccc
Austin Michael Regan <austinmregan@gmail.com>
2019-09-10 16:49:05 -0400

    axios can get data and render in curiosity

--- MartianWeatherAppMobile/src/components/Curiosity.js
+++ MartianWeatherAppMobile/src/components/Curiosity.js
@@ -7,14 +7,56 @@ import {
   Text,
   ImageBackground,
 } from 'react-native';
+import axios from 'axios';
+const curiositySearchUrl =
+  'https://pudding.cool/2017/12/mars-data/marsWeather.json';
 
 export default class CuriosityComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curiosityWeatherData: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(curiositySearchUrl)
+      .then(response => {
+        this.setState({
+          curiosityWeatherData: response.data,
+        });
+        console.log(this.state.curiosityWeatherData);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
   render() {
+    let curiosityWeather = this.state.curiosityWeatherData
+      .slice(0, 7)
+      .reverse()
+      .map((day, index) => {
+        if (index <= 6) {
+          day.min_temp = Math.round(1.8 * day.min_temp + 32);
+          day.max_temp = Math.round(1.8 * day.max_temp + 32);
+          return (
+            <View key={index}>
+              <Text>{day.sol}</Text>
+              <Text>{day.terrestrial_date}</Text>
+              <Text>{day.max_temp}°F</Text>
+              <Text>{day.min_temp}°F</Text>
+            </View>
+          );
+        }
+        return null;
+      });
     return (
       <View style={styles.curiosityContainer}>
         <Text>High</Text>
         <Text>Low</Text>
-        <View style={styles.dataContainer}></View>
+        <View style={styles.dataContainer}>
+          <View>{curiosityWeather}</View>
+        </View>
       </View>
     );
   }
