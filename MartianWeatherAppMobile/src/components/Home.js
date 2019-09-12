4c346699193808760977a59e267806726d105ebf
Austin Michael Regan <austinmregan@gmail.com>
2019-09-12 11:12:54 -0400

    imported shimmer, test for functionality

--- MartianWeatherAppMobile/src/components/Home.js
+++ MartianWeatherAppMobile/src/components/Home.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Shimmer from 'react-native-shimmer';
 
 export default class HomeComponent extends Component {
   render() {
     return (
       <View>
         <View style={styles.titleContainer}>
-          <Text style={styles.titleDescription}>4</Text>
+          <Shimmer>
+            <Text style={styles.titleDescription}>4</Text>
+          </Shimmer>
           <Text style={styles.superscript}>th</Text>
           <Text style={styles.titleDescription}> Rock Weather</Text>
         </View>
