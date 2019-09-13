20d062f7fffc07dfb5b36ce101f3d81a8a36f510
Austin Michael Regan <austinmregan@gmail.com>
2019-09-13 09:42:30 -0400

    added links for more infomratino to curiosity and inisght on about

--- MartianWeatherAppMobile/src/components/About.js
+++ MartianWeatherAppMobile/src/components/About.js
@@ -1,5 +1,5 @@
 import React, {Component, useState} from 'react';
-import {StyleSheet, Animated, Text, View} from 'react-native';
+import {StyleSheet, Animated, Text, View, Linking} from 'react-native';
 
 const FadeInView = props => {
   const [fadeAdmin] = useState(new Animated.Value(0)); // Initial value for opacity: 0
@@ -26,15 +26,39 @@ const FadeInView = props => {
 export default class HomeComponent extends Component {
   render() {
     return (
-      <FadeInView style={styles.flexToColumn}>
+      <FadeInView>
         <View style={styles.aboutContainer}>
           <Text style={styles.aboutDescription}>
             Fourth Rock Weather is a martian weather application using open
             source data provided by NASA
           </Text>
         </View>
-        <View style={styles.keyContainer}>
-          <Text style={styles.keyTitle}>Key</Text>
+        <View style={styles.flexToColumn}>
+          <View style={styles.keyContainer}>
+            <Text style={styles.keyTitle}>Key</Text>
+            <View>
+              <Text>Sol: Number of Martian Days since landing on Mars</Text>
+              <Text>Earth: Corresponding date on Earth</Text>
+              <Text>
+                H / F: High and Low temperatures for the given Sol in Fahrenheit
+              </Text>
+            </View>
+          </View>
+          <View>
+            <Text>Learn More About:</Text>
+            <Text
+              style={{color: 'blue'}}
+              onPress={() => Linking.openURL('https://mars.jpl.nasa.gov/msl/')}>
+              Curiosity
+            </Text>
+            <Text
+              style={{color: 'blue'}}
+              onPress={() =>
+                Linking.openURL('https://mars.nasa.gov/insight/weather/')
+              }>
+              InSight
+            </Text>
+          </View>
         </View>
       </FadeInView>
     );
@@ -43,13 +67,11 @@ export default class HomeComponent extends Component {
 
 const styles = StyleSheet.create({
   flexToColumn: {
-    display: "flex"
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'space-evenly',
     fontFamily: 'IBM Plex Sans',
   },
   aboutContainer: {
-    flexDirection: 'row',
     borderWidth: 2,
     borderColor: '#fbf7f5',
     padding: '5%',
@@ -61,7 +83,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   keyContainer: {
-    flexDirection: 'row',
     borderWidth: 2,
     borderColor: '#fbf7f5',
     padding: '5%',
